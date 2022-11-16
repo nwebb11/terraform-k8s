7607fd6fc1cbe06f47d3f5aedadf49888fa10553
Nathan Webb <45941246+nwebb11@users.noreply.github.com>
2022-11-16 17:05:03 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -24,30 +24,6 @@ resource "google_project_service" "compute" {
   disable_on_destroy = false
 }
 
-resource "google_compute_instance" "vm_instance" {
-  name         = "terraform-instance"
-  machine_type = "e2-micro"
-  tags         = ["allow-ssh"]
-
-  metadata = {
-    ssh-keys = "terraform:${file("./terraform-gcp-key.pub")}"
-  }
-
-  boot_disk {
-    initialize_params {
-      image = "ubuntu-os-cloud/ubuntu-2204-lts"
-      size  = 10
-    }
-  }
-
-  network_interface {
-    network = google_compute_network.vpc_network.self_link
-    access_config {
-      nat_ip = google_compute_address.static_ip.address
-    }
-  }
-}
-
 resource "google_compute_network" "vpc_network" {
   name                    = "terraform-network"
   auto_create_subnetworks = "true"
@@ -66,5 +42,46 @@ resource "google_compute_firewall" "allow_ssh" {
   allow {
     protocol = "tcp"
     ports    = ["22"]
+  }
+}
+
+resource "google_compute_instance" "vm_instance" {
+  name         = "terraform-instance"
+  machine_type = "e2-micro"
+  tags         = ["allow-ssh"]
+
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-os-cloud/ubuntu-2204-lts"
+      size  = 10
+    }
+  }
+
+  network_interface {
+    network = google_compute_network.vpc_network.self_link
+    access_config {
+      nat_ip = google_compute_address.static_ip.address
+    }
+  }
+
+  provisioner "remote-exec" {
+    connection {
+      host        = google_compute_address.static_ip.address
+      type        = "ssh"
+      user        = var.ssh_user
+      timeout     = "600s"
+      private_key = file(var.ssh_priv_key)
+    }
+    inline = [
+      "sudo apt update",
+      "sudo DEBIAN_FRONTEND=noninteractive apt upgrade -y",
+      "sudo apt autoremove -y"
+    ]
+  }
+
+  depends_on = [google_compute_firewall.allow_ssh]
+
+  metadata = {
+    ssh-keys = "${var.ssh_user}:${file(var.ssh_pub_key)}"
   }
 }
