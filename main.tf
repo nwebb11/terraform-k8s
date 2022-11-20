7ad0a1d7d7ead232007eeffe8eb898e23097ca11
nwebb11 <nathanwebb.j.h@gmail.com>
2022-11-20 22:00:42 +0000

    Add AWS folder

--- main.tf
+++
@@ -1,9 +0,0 @@
-resource "google_project_service" "cloud_resource_manager" {
-  service            = "cloudresourcemanager.googleapis.com"
-  disable_on_destroy = false
-}
-
-resource "google_project_service" "compute" {
-  service            = "compute.googleapis.com"
-  disable_on_destroy = false
-}
